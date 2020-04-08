897b3093ebe1de97fa3aa4ae877de827ad49b38a
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2020-04-08 03:03:14 -0300

    ajuste

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
+// server.js
 const express = require('express');
-const path = require('path');
 const app = express();
-
-// Serve static files....
-app.use(express.static(__dirname + '/dist/adwords-ui'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/adwords-ui/index.html'));
-});
-
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
